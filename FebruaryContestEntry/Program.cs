c45d33f22f78eea8de0f136a68b2b5fb361a7a61
Md Shiefuzzaman <shiefuzzaman.bd@gmail.com>
2020-02-14 20:42:05 -0600

    Print shape

--- FebruaryContestEntry/Program.cs
+++ FebruaryContestEntry/Program.cs
@@ -4,17 +4,36 @@ namespace FebruaryContestEntry
 {
 	class Program
 	{
-		const char block = '▓';
+		const char block = '▓';		
+		const int columns = 30;
+		const int rows = 15;
+		const int size = rows * columns;
 
 		static void Main(string[] args)
 		{
 			// The solution implementation goes here
-
+			int counter = 0;
+			while(counter < size)
+			{ 				
+				PrintBlock(counter % columns, GetConsoleColor(counter % columns, counter / columns));
+				counter++;
+			}
 			Console.ResetColor();
 			Console.WriteLine();
 			Console.WriteLine();
 			Console.WriteLine("Yay, a colorful shape!");
 			Console.ReadLine();
+		}
+		private static ConsoleColor GetConsoleColor(int xCordinate, int yCordinate)
+			=> (xCordinate == 0 || xCordinate == columns - 1 || yCordinate == 0 || yCordinate == rows - 1)
+			? ConsoleColor.Green
+			: ConsoleColor.Yellow;
+		private static void PrintBlock(int xCoordinate, ConsoleColor blockColor)
+		{
+			Console.ForegroundColor = blockColor;
+			Console.Write(block);
+			if (xCoordinate == columns - 1)
+				Console.WriteLine();
 		}
 	}
 }
